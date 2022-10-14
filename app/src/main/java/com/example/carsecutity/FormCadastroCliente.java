8769c67a1b8aec3506025cf4d733677b8a01460e
GedelQ <gedelqs@gmail.com>
2022-10-13 23:58:03 -0300

    Form cadastro de cliente

---
+++ app/src/main/java/com/example/carsecutity/FormCadastroCliente.java
@@ -0,0 +1,14 @@
+package com.example.carsecutity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormCadastroCliente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_cadastro_cliente);
+    }
+}
