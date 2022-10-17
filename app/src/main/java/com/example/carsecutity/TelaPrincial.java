ad1160e267efbd655bf95a6a3e7e28f25c4ae727
GedelQ <gedelqs@gmail.com>
2022-10-16 21:44:38 -0300

    Complementos da tela de cadastro. Atualização dos botões. Tela principal.

---
+++ app/src/main/java/com/example/carsecutity/TelaPrincial.java
@@ -0,0 +1,14 @@
+package com.example.carsecutity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaPrincial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_princial);
+    }
+}
