8009de75e5d7fa76ea49f5774b4295115afd63d1
GedelQ <gedelqs@gmail.com>
2022-10-20 23:57:21 -0300

    Conclusão da tela principal.

--- app/src/main/java/com/example/carsecutity/TelaPrincial.java
+++ app/src/main/java/com/example/carsecutity/TelaPrincial.java
@@ -10,5 +10,6 @@ public class TelaPrincial extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_princial);
+        getSupportActionBar().hide();
     }
 }
