34e2e947431a3390b4bbd05a4103097a9a92cc74
GedelQ <gedelqs@gmail.com>
2022-10-21 21:41:08 -0300

    Criação de todas as telas principais.

---
+++ app/src/main/java/com/example/carsecutity/AssinarPlano.java
@@ -0,0 +1,14 @@
+package com.example.carsecutity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AssinarPlano extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assinar_plano);
+    }
+}
---
+++ app/src/main/java/com/example/carsecutity/ContatarSeguradora.java
@@ -0,0 +1,14 @@
+package com.example.carsecutity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContatarSeguradora extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contatar_seguradora);
+    }
+}
