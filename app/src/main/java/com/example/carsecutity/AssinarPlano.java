73fedb44ecc3cf4be2ab5bfe0f9e5b850449a12b
GedelQ <gedelqs@gmail.com>
2022-10-26 00:09:15 -0300

    Tela de seleção de seguro

--- app/src/main/java/com/example/carsecutity/AssinarPlano.java
+++ app/src/main/java/com/example/carsecutity/AssinarPlano.java
@@ -10,5 +10,6 @@ public class AssinarPlano extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assinar_plano);
+        getSupportActionBar().hide();
     }
 }
--- app/src/main/java/com/example/carsecutity/TelaPrincipal.java
+++ app/src/main/java/com/example/carsecutity/TelaPrincipal.java
@@ -13,6 +13,8 @@ public class TelaPrincipal extends AppCompatActivity {
     private Button bt_edit_perfil;
     private Button bt_contatar;
     private Button bt_gps;
+    private Button bt_assinar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,12 +56,23 @@ public class TelaPrincipal extends AppCompatActivity {
             }
         });
 
+
+        bt_assinar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TelaPrincipal.this, AssinarPlano.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 
     private void IniciarComponents() {
         bt_sair = findViewById(R.id.bt_sair);
         bt_edit_perfil = findViewById(R.id.bt_edit_perfil);
         bt_contatar = findViewById(R.id.bt_contatar);
         bt_gps = findViewById(R.id.bt_gps);
+        bt_assinar = findViewById(R.id.bt_assinar);
     }
 }
